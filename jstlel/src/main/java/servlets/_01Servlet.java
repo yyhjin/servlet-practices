5ba18a034b5534dcaaa0a2160324c22d5021ede9
yyhjin <wendy0301@naver.com>
2023-09-11 14:52:41 +0900

    jstl&el #2

--- jstlel/src/main/java/servlets/_01Servlet.java
+++ jstlel/src/main/java/servlets/_01Servlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +15,35 @@ public class _01Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 값
 		int iVal = 10;
+		long lVal = 10L;
+		float fVal = 3.14f;
+		boolean bVal = true;
+		String sVal = "가나다라마바사";
+		
+		// 객체
+		Object o = null;
+		
+		UserVo userVo = new UserVo();
+		userVo.setNo(10L);
+		userVo.setName("둘리");
+		
+		request.setAttribute("obj", o);
+		request.setAttribute("vo", userVo);
+		
+		// Map 객체
+		Map<String, Object> m = new HashMap<>();
+		m.put("ival", iVal);
+		m.put("fval", fVal);
+		m.put("sval", sVal);
+		
+		request.setAttribute("map", m);
 		
 		request.setAttribute("ival", iVal);
+		request.setAttribute("lval", lVal);
+		request.setAttribute("fval", fVal);
+		request.setAttribute("bval", bVal);
+		request.setAttribute("sval", sVal);
+		
 		request
 			.getRequestDispatcher("/WEB-INF/views/01.jsp")
 			.forward(request, response);
--- jstlel/src/main/webapp/WEB-INF/views/01.jsp
+++ jstlel/src/main/webapp/WEB-INF/views/01.jsp
@@ -7,6 +7,45 @@
 <title>Insert title here</title>
 </head>
 <body>
-	${ival }
+	<h4>값출력</h4>
+	${ival } <br>
+	${lval } <br>
+	${fval } <br>
+	${bval } <br>
+	${sval } <br>
+
+	<h4>객체 출력</h4>
+	---${obj }--- <br>
+	${vo.no } <br>
+	${vo.name } <br>	
+	
+	<h4>map 객체 출력</h4>
+	${map.ival } <br>
+	${map.fval } <br>
+	${map.sval } <br>
+	
+	<h4>산술연산</h4>
+	${3*4+6/2 } <br>
+	${ival + 10 } <br>
+	
+	<h4>관계연산</h4>
+	${ival == 10 } <br>
+	${ival < 5 } <br>
+	${obj == null } <br>
+	${obj != null } <br>
+	${empty obj } <br>
+	${not empty obj } <br>
+	
+	<h4>논리연산</h4>
+	${ival == 10 && lval < 1000 } <br>
+	${ival == 10 || lval < 1000 } <br>
+	
+	<h4>요청 파라미터</h4>
+	${param.a + 10 } <br>
+	${param.email } <br>
+	
+	<h4>Context Path</h4>
+	${pageContext.request.contextPath }
+	
 </body>
 </html>
